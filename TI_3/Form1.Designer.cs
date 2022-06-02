cd2cec26bf61447ecef3f5f6087ca01c7a4bcd6e
DashaTsybulkina <cybulkinadara@gmail.com>
2022-06-02 22:01:10 +0300

    Добавьте файлы проекта.

---
+++ TI_3/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace TI_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_p = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_q = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox_K = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_r = new System.Windows.Forms.TextBox();
+            this.textBox_ks = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox_m = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox_n = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox_ko = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox_p
+            // 
+            this.textBox_p.Location = new System.Drawing.Point(173, 72);
+            this.textBox_p.Name = "textBox_p";
+            this.textBox_p.Size = new System.Drawing.Size(178, 27);
+            this.textBox_p.TabIndex = 0;
+            this.textBox_p.TextChanged += new System.EventHandler(this.textBox_p_TextChanged);
+            this.textBox_p.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_p_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите p:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(173, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Введите q:";
+            // 
+            // textBox_q
+            // 
+            this.textBox_q.Location = new System.Drawing.Point(173, 130);
+            this.textBox_q.Name = "textBox_q";
+            this.textBox_q.Size = new System.Drawing.Size(174, 27);
+            this.textBox_q.TabIndex = 3;
+            this.textBox_q.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_q_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(426, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Зашифровать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(426, 430);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 29);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Расшифровать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(580, 43);
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(334, 222);
+            this.textBoxInput.TabIndex = 6;
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(580, 318);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(334, 222);
+            this.textBoxOutput.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 47);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 33);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Открыть файл";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox_K
+            // 
+            this.textBox_K.Location = new System.Drawing.Point(173, 194);
+            this.textBox_K.Name = "textBox_K";
+            this.textBox_K.Size = new System.Drawing.Size(174, 27);
+            this.textBox_K.TabIndex = 9;
+            this.textBox_K.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_K_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(173, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Введите K(секретный):";
+            // 
+            // textBox_r
+            // 
+            this.textBox_r.Location = new System.Drawing.Point(169, 333);
+            this.textBox_r.Name = "textBox_r";
+            this.textBox_r.Size = new System.Drawing.Size(178, 27);
+            this.textBox_r.TabIndex = 11;
+            this.textBox_r.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_r_KeyPress);
+            // 
+            // textBox_ks
+            // 
+            this.textBox_ks.Location = new System.Drawing.Point(173, 386);
+            this.textBox_ks.Name = "textBox_ks";
+            this.textBox_ks.Size = new System.Drawing.Size(174, 27);
+            this.textBox_ks.TabIndex = 12;
+            this.textBox_ks.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_ks_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(169, 363);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(165, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Введите K(секретный):";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(169, 310);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Введите r:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(580, 20);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(121, 20);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Исходный текст:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(580, 295);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(122, 20);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Конечный текст:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 311);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 29);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Открыть файл";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox_m
+            // 
+            this.textBox_m.Location = new System.Drawing.Point(385, 72);
+            this.textBox_m.Name = "textBox_m";
+            this.textBox_m.ReadOnly = true;
+            this.textBox_m.Size = new System.Drawing.Size(178, 27);
+            this.textBox_m.TabIndex = 18;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(385, 49);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(127, 20);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Функция Эйлера:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(385, 107);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(23, 20);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "N:";
+            // 
+            // textBox_n
+            // 
+            this.textBox_n.Location = new System.Drawing.Point(385, 130);
+            this.textBox_n.Name = "textBox_n";
+            this.textBox_n.ReadOnly = true;
+            this.textBox_n.Size = new System.Drawing.Size(178, 27);
+            this.textBox_n.TabIndex = 20;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(385, 171);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(88, 20);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "K(откртый):";
+            // 
+            // textBox_ko
+            // 
+            this.textBox_ko.Location = new System.Drawing.Point(385, 194);
+            this.textBox_ko.Name = "textBox_ko";
+            this.textBox_ko.ReadOnly = true;
+            this.textBox_ko.Size = new System.Drawing.Size(178, 27);
+            this.textBox_ko.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(926, 552);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox_ko);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox_n);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox_m);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_ks);
+            this.Controls.Add(this.textBox_r);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_K);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_q);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_p);
+            this.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox_p;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox_q;
+        private Button button1;
+        private Button button2;
+        private TextBox textBoxInput;
+        private TextBox textBoxOutput;
+        private Button button3;
+        private OpenFileDialog openFileDialog1;
+        private TextBox textBox_K;
+        private Label label3;
+        private TextBox textBox_r;
+        private TextBox textBox_ks;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Button button4;
+        private TextBox textBox_m;
+        private Label label8;
+        private Label label9;
+        private TextBox textBox_n;
+        private Label label10;
+        private TextBox textBox_ko;
+    }
+}
---
+++ TI_3/Form1.cs
@@ -0,0 +1,319 @@
+﻿using System.Numerics;
+using System.Security.Cryptography;
+
+namespace TI_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string s = "";
+        string fileName;
+        byte[] byteArray;
+        byte[] byteArrayOut;
+        string extension;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((textBox_p.Text.Length > 0) && (textBox_q.Text.Length > 0) && (textBox_K.Text.Length > 0) )
+            {
+                BigInteger p = BigInteger.Parse(textBox_p.Text);
+                BigInteger q = BigInteger.Parse(textBox_q.Text);
+                BigInteger K = BigInteger.Parse(textBox_K.Text);
+                if (MillerRabinTest(p, 100) && MillerRabinTest(q, 100))
+                {
+                    if (p == q)
+                    {
+                        MessageBox.Show("Числа должны быть разные!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+                    BigInteger n;
+                    try
+                    {
+                        n = p * q;
+                        ushort.Parse(n.ToString());
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Числа больше чем 2 байта!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+                    BigInteger m = (p - 1) * (q - 1);
+                    BigInteger ko;
+                    if (findNodEvclid(K, m)!=1)
+                    {
+                        MessageBox.Show("Функция эйлера и ключ должны быть взаимно простыми!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+                    ko = Calculate_ko(m, K);
+                    if (ko > 1 && ko < m && p>0 && q>0)
+                    {
+                        textBox_n.Text = n.ToString();
+                        textBox_m.Text = m.ToString();
+                        textBox_ko.Text = ko.ToString();
+                        byte[] encodedBytes = textBoxInput.Text.Split(' ').Select(byte.Parse).ToArray();
+                        int[] Cipher = new int[encodedBytes.Length];
+                        for (int i = 0; i < encodedBytes.Length; i++)
+                        {
+                            string temp1 = encodedBytes[i].ToString();
+                            BigInteger result = BigInteger.Parse(temp1);
+                            Cipher[i] = ((int)BinaryPower(result, ko, n));
+                        }
+                        int temp = fileName.IndexOf(extension);
+                        string pathout = fileName.Remove(temp, extension.Length);
+                        pathout = pathout + "Output" + extension;
+                        writeCipheredWord(pathout, Cipher);
+                        textBoxOutput.Text = string.Join(" ", Cipher);
+                    }
+                    else {
+                        MessageBox.Show("Неверные данные");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Неверные данные");
+                }
+            }
+        }
+
+        BigInteger findNodEvclid(BigInteger m, BigInteger n) {
+            while (m != n)
+            {
+                if (m > n)
+                {
+                    m = m - n;
+                }
+                else
+                {
+                    n = n - m;
+                }
+            }
+            return n;
+        }
+
+        void writeCipheredWord(string path, int[] Cipher)
+        {
+            byte[] arr = new byte[Cipher.Length * 2];
+            int j = -1;
+            for (int i = 0; i < Cipher.Length; i++)
+            {
+                arr[++j] = (byte)(Cipher[i] >> 8);
+                arr[++j] = (byte)(Cipher[i] & 255);
+            }
+            File.WriteAllBytes(path, arr);
+        }
+
+        private int BinaryPower(BigInteger basement, BigInteger exp, BigInteger mod)
+        {
+            BigInteger test;
+            for (test = 1; exp > 0; exp >>= 1)
+            {
+                if ((exp & 1) == 1)
+                    test = ((test % mod) * (basement % mod)) % mod;
+                basement = ((basement % mod) * (basement % mod)) % mod;
+            }
+            return (int)test;
+        }
+
+        private BigInteger Calculate_ko(BigInteger m, BigInteger K)
+        {
+            if ((K % m != 0) || (m % K != 0))
+            {
+                BigInteger d0 = m;
+                BigInteger d1 = K;
+                BigInteger x0 = 1;
+                BigInteger y0 = 0;
+                BigInteger x1 = 0;
+                BigInteger y1 = 1;
+                BigInteger q;
+                BigInteger d2;
+                BigInteger x2;
+                BigInteger y2;
+                while (d1 > 1)
+                {
+                    q = d0 / d1;
+                    d2 = d0 % d1;
+                    x2 = x0 - q * x1;
+                    y2 = y0 - q * y1;
+                    d0 = d1;
+                    d1 = d2;
+                    x0 = x1;
+                    x1 = x2;
+                    y0 = y1;
+                    y1 = y2;
+                }
+                if (y1 < 0)
+                {
+                    y1 += m;
+                }
+                return y1;
+            }
+            else
+            {
+                MessageBox.Show("Неверные данные");
+            }
+            return -1;
+        }
+
+        public bool MillerRabinTest(BigInteger n, int k)
+        {
+            if (n == 2 || n == 3)
+                return true;
+            if (n < 2 || n % 2 == 0)
+                return false;
+            BigInteger t = n - 1;
+            int s = 0;
+            while (t % 2 == 0)
+            {
+                t /= 2;
+                s += 1;
+            }
+            for (int i = 0; i < k; i++)
+            {
+                RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+                byte[] _a = new byte[n.ToByteArray().LongLength];
+                BigInteger a;
+                do
+                {
+                    rng.GetBytes(_a);
+                    a = new BigInteger(_a);
+                }
+                while (a < 2 || a >= n - 2);
+                BigInteger x = BigInteger.ModPow(a, t, n);
+                if (x == 1 || x == n - 1)
+                    continue;
+                for (int r = 1; r < s; r++)
+                {
+                    x = BigInteger.ModPow(x, 2, n);
+                    if (x == 1)
+                        return false;
+                    if (x == n - 1)
+                        break;
+                }
+                if (x != n - 1)
+                    return false;
+            }
+            return true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            textBoxInput.Text ="";
+            textBoxOutput.Text = "";
+            fileName = openFileDialog1.FileName;
+            byte[] a = File.ReadAllBytes(fileName);
+            textBoxInput.Text = String.Join(" ", a);
+             extension = Path.GetExtension(fileName);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if ((textBox_r.Text.Length > 0) && (textBox_ks.Text.Length > 0))
+            {
+                BigInteger r = BigInteger.Parse(textBox_r.Text);
+                try
+                {
+                    ushort.Parse(r.ToString());
+                }
+                catch
+                {
+                    MessageBox.Show("r больше чем 2 байта!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                BigInteger ks = BigInteger.Parse(textBox_ks.Text);
+                if (ks > 1 && ks < r)
+                {
+                    int[] ints = textBoxInput.Text.Split(' ').Select(int.Parse).ToArray();
+                    byte[] Cipher = new byte[ints.Length];
+                    for (int i = 0; i < ints.Length; i++)
+                    {
+                        string temp1 = ints[i].ToString();
+                        BigInteger result = BigInteger.Parse(temp1);
+                        Cipher[i] = (byte)BinaryPower(result, ks, r);
+                    }
+                    textBoxOutput.Text = String.Join(" ", Cipher);
+                    int temp = fileName.IndexOf(extension);
+                    string pathout = fileName.Remove(temp, extension.Length);
+                    pathout = pathout + "Output" + extension;
+                    byte[] OutText = new byte[Cipher.Length];
+                    OutText = Cipher.Select(x => (byte)x).ToArray();
+                    File.WriteAllBytes(pathout, OutText);
+                }
+                else {
+                    MessageBox.Show("Неверные данные!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            fileName = openFileDialog1.FileName;
+            byte[] arr = File.ReadAllBytes(fileName);
+            long size = new FileInfo(fileName).Length;
+             size /= 2;
+            int[] result = new int[size];
+            int j = -1;
+            for (int i = 0; i < size; i++)
+                result[i] = (arr[++j] << 8) + arr[++j];
+            textBoxInput.Text = string.Join(" ", result);
+            extension = Path.GetExtension(fileName);
+        }
+
+        private void textBox_p_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox_q_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox_K_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox_r_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox_ks_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox_p_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
